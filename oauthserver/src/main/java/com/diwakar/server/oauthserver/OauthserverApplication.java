20e1f39c2917992efc29982be46f80f4ca53a868
Diwakar Shukla <csediwakar@gmail.com>
2020-02-14 15:19:22 +0530

    Server Init Code

--- oauthserver/src/main/java/com/diwakar/server/oauthserver/OauthserverApplication.java
+++ oauthserver/src/main/java/com/diwakar/server/oauthserver/OauthserverApplication.java
@@ -2,8 +2,10 @@ package com.diwakar.server.oauthserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 public class OauthserverApplication {
 
     public static void main(String[] args) {

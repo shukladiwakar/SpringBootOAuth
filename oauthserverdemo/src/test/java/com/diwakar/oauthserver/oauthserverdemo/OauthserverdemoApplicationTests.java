45042f6a60ee38864ab26a5c410da56fe872ada3
ds55291 <shukladiwakar@johndeere.com>
2020-02-14 14:33:43 +0530

    Adding the Initial Server Code

---
+++ oauthserverdemo/src/test/java/com/diwakar/oauthserver/oauthserverdemo/OauthserverdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diwakar.oauthserver.oauthserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthserverdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
